3191afa076a8828cf53ae1ede88997536aa5aba0
Monika Barget <monika.barget@mu.ie>
2020-07-29 17:59:20 +0200

    Add files via upload

---
+++ qgis2web_IEG_jubilee_map/layers/Museen_3.js
@@ -0,0 +1,9 @@
+var json_Museen_3 = {
+"type": "FeatureCollection",
+"name": "Museen_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "label": "49: Deutsches Museum, München", "institute": "Deutsches Museum", "place": "München", "mix": "Deutsches Museum", "Geisteswissenschaft": "Deutsches Museum" }, "geometry": { "type": "Point", "coordinates": [ 11.583366394042969, 48.129992929501768 ] } },
+{ "type": "Feature", "properties": { "label": "59: Germanisches Museum, Nürnberg", "institute": "Germansiches Museum", "place": "Nürnberg", "mix": "Germanisches Museum", "Geisteswissenschaft": "Germanisches Museum" }, "geometry": { "type": "Point", "coordinates": [ 11.076192855834959, 49.447858200992236 ] } }
+]
+}
---
+++ qgis2web_IEG_jubilee_map/layers/SonstigeForschungsinstitute_4.js
@@ -0,0 +1,12 @@
+var json_SonstigeForschungsinstitute_4 = {
+"type": "FeatureCollection",
+"name": "SonstigeForschungsinstitute_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "label": "51: Deutsche Forschungshochschule, Berlin", "institute": "Deutsche Forschungshochschule", "place": "Berlin", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 13.281998634338379, 52.459042843502665 ] } },
+{ "type": "Feature", "properties": { "label": "53: Institut für Wirtschaftsforschung e. V., München", "institute": "Institut für Wirtschaftsforschung e. V.", "place": "München", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 11.603896021842957, 48.122648917132146 ] } },
+{ "type": "Feature", "properties": { "label": "52: Hamburgisches Weltwirtschaftsarchiv, Hamburg", "institute": "Hamburgisches Weltwirtschaftsarchiv", "place": "Hamburg", "mix": "Hamburgisches Weltwirtschaftsarchiv" }, "geometry": { "type": "Point", "coordinates": [ 9.991248697042465, 53.552060136255754 ] } },
+{ "type": "Feature", "properties": { "label": "54: Westdeutsche Bibliothek, Marburg", "institute": "Westdeutsche Bibliothek", "place": "Marburg", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 8.770748376846313, 50.80735852135696 ] } },
+{ "type": "Feature", "properties": { "label": "55: Notgemeinschaft der deutschen Wissenschaft, Bonn", "institute": "Notgemeinschaft der deutschen Wissenschaft ", "place": "Bonn", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 7.151756286621094, 50.683842649833153 ] } }
+]
+}
