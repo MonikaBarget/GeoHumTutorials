101b4bae8ca80a16ff0505e69b38ba48618a30fc
Monika Barget <monika.barget@mu.ie>
2020-08-04 12:53:11 +0200

    Update SonstigeForschungsinstitute_4.js

--- qgis2web_IEG_jubilee_map/layers/SonstigeForschungsinstitute_4.js
+++ qgis2web_IEG_jubilee_map/layers/SonstigeForschungsinstitute_4.js
@@ -7,7 +7,6 @@ var json_SonstigeForschungsinstitute_4 = {
 { "type": "Feature", "properties": { "label": "53", "institute": "Institut für Wirtschaftsforschung e. V.", "place": "München", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 11.603896021842957, 48.122648917132146 ] } },
 { "type": "Feature", "properties": { "label": "52", "institute": "Hamburgisches Weltwirtschaftsarchiv", "place": "Hamburg", "mix": "Hamburgisches Weltwirtschaftsarchiv" }, "geometry": { "type": "Point", "coordinates": [ 9.991248697042465, 53.552060136255754 ] } },
 { "type": "Feature", "properties": { "label": "54", "institute": "Westdeutsche Bibliothek", "place": "Marburg", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 8.770748376846313, 50.80735852135696 ] } },
-{ "type": "Feature", "properties": { "label": "55", "institute": "Notgemeinschaft der deutschen Wissenschaft ", "place": "Bonn", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 7.151756286621094, 50.683842649833153 ] } },
 { "type": "Feature", "properties": { "label": "35", "institute": "Notgemeinschaft der deutschen Wissenschaft", "place": "Bonn", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 7.151756286621094, 50.683842649833153 ] } }
 ]
 }
