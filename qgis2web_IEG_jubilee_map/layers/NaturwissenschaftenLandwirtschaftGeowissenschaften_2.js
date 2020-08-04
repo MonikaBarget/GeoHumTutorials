cb8cae0f1810d324f4e1482c5ca44fd34b2390da
Monika Barget <monika.barget@mu.ie>
2020-08-04 12:47:20 +0200

    3 institutes moved

--- qgis2web_IEG_jubilee_map/layers/NaturwissenschaftenLandwirtschaftGeowissenschaften_2.js
+++ qgis2web_IEG_jubilee_map/layers/NaturwissenschaftenLandwirtschaftGeowissenschaften_2.js
@@ -3,12 +3,11 @@ var json_NaturwissenschaftenLandwirtschaftGeowissenschaften_2 = {
 "name": "NaturwissenschaftenLandwirtschaftGeowissenschaften_2",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "label": "33", "institute": "Vogelwarte Radolfzell", "place": "Radolfzell", "mix": "Vogelwarte Radolfzell" }, "geometry": { "type": "Point", "coordinates": [ 8.996375799179077, 47.766214652648365 ] } },
-{ "type": "Feature", "properties": { "label": "34", "institute": "Deutsche Forschungsanstalt für Psychiatrie", "place": "München", "mix": "Deutsche Forschungsanstalt für Psychiatrie" }, "geometry": { "type": "Point", "coordinates": [ 11.576306819915771, 48.173870393545009 ] } },
-{ "type": "Feature", "properties": { "label": "35", "institute": "Forschungsstelle für Leder und Eiweiß", "place": "Regensburg", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 12.094230651855469, 49.025037375377813 ] } },
+{ "type": "Feature", "properties": { "label": "38", "institute": "Vogelwarte Radolfzell", "place": "Radolfzell", "mix": "Vogelwarte Radolfzell" }, "geometry": { "type": "Point", "coordinates": [ 8.996375799179077, 47.766214652648365 ] } },
+{ "type": "Feature", "properties": { "label": "46", "institute": "Deutsche Forschungsanstalt für Psychiatrie", "place": "München", "mix": "Deutsche Forschungsanstalt für Psychiatrie" }, "geometry": { "type": "Point", "coordinates": [ 11.576306819915771, 48.173870393545009 ] } },
+{ "type": "Feature", "properties": { "label": "48", "institute": "Forschungsstelle für Leder und Eiweiß", "place": "Regensburg", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 12.094230651855469, 49.025037375377813 ] } },
 { "type": "Feature", "properties": { "label": "36", "institute": "Kerkhoff-Institut", "place": "Bad Nauheim", "mix": "Kerkhoff-Institut" }, "geometry": { "type": "Point", "coordinates": [ 8.743057250976561, 50.365513532398737 ] } },
 { "type": "Feature", "properties": { "label": "37", "institute": "Paul-Ehrlich-Institut, Staatliche Anstalt für experimentelle Therapie und Georg-Speyer-Haus", "place": "Frankfurt", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 8.665230274200439, 50.09515669615184 ] } },
-{ "type": "Feature", "properties": { "label": "38", "institute": "Gmelin-Institut für anorganische Chemie und Grenzgebiete in der Max-Plank-Gesellschaft", "place": "Clausthal-Zellerfeld", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 10.362526774406433, 51.804455333940552 ] } },
 { "type": "Feature", "properties": { "label": "39", "institute": "Akademie für Raumforschung und Landesplanung", "place": "Hannover", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 9.73363995552063, 52.412033589550411 ] } },
 { "type": "Feature", "properties": { "label": "40", "institute": "Reichsamt für Bodenforschung, Abteilung Erdölforschung", "place": "Celle", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 10.082015991210938, 52.62431053731467 ] } },
 { "type": "Feature", "properties": { "label": "41", "institute": "Reichsamt für Bodenforschung", "place": "Hannover", "mix": "Reichsamt für Bodenforschung" }, "geometry": { "type": "Point", "coordinates": [ 9.822898507118225, 52.404084745713071 ] } },
@@ -17,7 +16,5 @@ var json_NaturwissenschaftenLandwirtschaftGeowissenschaften_2 = {
 { "type": "Feature", "properties": { "label": "44", "institute": "Tuberculose-Forschungsinstitut", "place": "Borstel", "mix": "Tuberculose-Forschungsinstitut" }, "geometry": { "type": "Point", "coordinates": [ 10.197077393531799, 53.819765010533942 ] } },
 { "type": "Feature", "properties": { "label": "45", "institute": "Institut für Weltwirtschaft", "place": "Kiel", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 10.156173706054686, 54.338680705010987 ] } },
 { "type": "Feature", "properties": { "label": "47", "institute": "Astronomisches Recheninstitut", "place": "Heidelberg", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 8.687862753868103, 49.417754276955151 ] } },
-{ "type": "Feature", "properties": { "label": "46", "institute": "Gmelin-Institut für anorganische Chemie und Grenzbetriebe", "place": "Tübingen", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 9.049816131591797, 48.527838974114474 ] } },
-{ "type": "Feature", "properties": { "label": "48", "institute": "Notgemeinschaft der deutschen Wissenschaft ", "place": "Bonn", "mix": null }, "geometry": { "type": "Point", "coordinates": [ 7.151756286621094, 50.683842649833153 ] } }
 ]
 }
